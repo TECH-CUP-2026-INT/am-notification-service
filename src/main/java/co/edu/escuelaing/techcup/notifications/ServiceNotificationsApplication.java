909531ebd397822a50793723a3331292996a15b3
julianizz18 <904juliancorredor@gmail.com>
2026-07-07 23:47:33 -0500

    Realizacion del repo

---
+++ src/main/java/co/edu/escuelaing/techcup/notifications/ServiceNotificationsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.techcup.notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceNotificationsApplication.class, args);
+	}
+
+}
